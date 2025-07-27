f3739a6de8636b34d8c28acc8df79655275d3a78
shawn <shawnhutchins1@gmail.com>
2025-07-27 15:59:07 -0400

    Challenge 4 added control variables

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from turtle import Turtle, Screen
 import random
 
 timmy = Turtle()
+#figure out how to control turtle speed
+timmy.speed(10)
 screen = Screen()
 
 #make a list of colors to chose from
+colors = ["red", "purple3", "LimeGreen", "DeepSkyBlue2", "gold1", "tan3"]
 #make a variable to control line width
+line_width = 10
 #make a variable to control line length
+line_length = 50
 #make a variable to control line color
+line_color = random.choice(colors)
 #make a variable to control number or iterations
+iterations = 30
 
-#figure out how to control turtle speed
 #make a function to randomly pick an int from -1 to 1
 #make variables for x and y axis
 #randomly pick x or y axis
