218e7863d4ac72abf844208fe83bfad3f99674dc
shawn <shawnhutchins1@gmail.com>
2025-07-27 02:04:27 -0400

    Challenge 3 finished

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from turtle import Turtle, Screen
+from random import randint
 
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("red")
 
-sides = 3
-side_len = 100
-# 360 / sides = angle
-
-
-for _ in range(20):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
-
 screen = Screen()
+screen.colormode(255)
+
+sides = 3
+max_sides = 10
+side_len = 100
+
+def new_random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    new_color = [r, g, b]
+    return new_color
+
+line_color = new_random_color()
+
+while sides != max_sides:
+    timmy.color((line_color[0], line_color[1], line_color[2]))
+    for _ in range(sides):
+        timmy.forward(100)
+        timmy.right(360/sides)
+    sides += 1
+    line_color = new_random_color()
+
 screen.exitonclick()
