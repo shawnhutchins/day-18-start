f05261dcc6861d473d092185ffb9b52e7762dc35
shawn <shawnhutchins1@gmail.com>
2025-07-30 19:58:27 -0400

    Final challenge extracting colors from jpg

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ timmy = t.Turtle()
 
 colors = colorgram.extract("image.jpg", 20)
 t_colors = []
-pprint.pp(colors[0].rgb.r)
+
 for index in range(len(colors)):
-    colors[index].rgb.r
+    r = colors[index].rgb.r
+    g = colors[index].rgb.g
+    b = colors[index].rgb.b
+    t_colors.append((r, g, b))
 
 screen = t.Screen()
 screen.exitonclick()
