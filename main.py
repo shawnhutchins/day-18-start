cd17c6d187f46e23402e8259c12d3741e86ec0b3
shawn <shawnhutchins1@gmail.com>
2025-07-30 21:14:11 -0400

    Final challenge Hirst painting finished

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import turtle as t
-import colorgram
-import pprint
+import random
 
 t.colormode(255)
 timmy = t.Turtle()
 
-colors = colorgram.extract("image.jpg", 20)
-t_colors = []
+colors = [(235, 39, 116), (216, 163, 60), (141, 26, 67), (237, 70, 38), (14, 142, 90), (176, 162, 48), (177, 41, 94),
+          (33, 122, 187), (54, 189, 226), (242, 219, 56), (78, 22, 71), (128, 190, 100), (198, 63, 37), (26, 169, 124),
+          (248, 224, 1), (201, 137, 167)]
 
-for index in range(len(colors)):
-    r = colors[index].rgb.r
-    g = colors[index].rgb.g
-    b = colors[index].rgb.b
-    t_colors.append((r, g, b))
+x = -225
+y = -225
+offset = 50
+dot_size = 20
+
+for _ in range(10):
+    for _ in range(10):
+        timmy.teleport(x, y)
+        timmy.dot(dot_size, random.choice(colors))
+        x += offset
+    x = -225
+    y += offset
 
 screen = t.Screen()
 screen.exitonclick()
